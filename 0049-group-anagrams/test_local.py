2bc09c9f1b015b2c84e0e837a2b136ddf181b489
Melisov01 <aytemirmelisov@gmail.com>
2026-02-09 15:48:42 +0300

    solve 0046 group anagrams task

---
+++ 0049-group-anagrams/test_local.py
@@ -0,0 +1,11 @@
+from solution import Solution
+
+def main():
+    sol = Solution()
+
+    test_input = ["act","pots","tops","cat","stop","hat"]
+    print(sol.groupAnagrams(test_input))
+
+
+if __name__ == "__main__":
+    main()
